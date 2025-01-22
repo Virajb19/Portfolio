11c0856f2914cb5f88b7f064ff71e6669d4743eb
Virajb19 <virajb853@gmail.com>
2025-01-22 18:35:56 +0530

    Added trpc and redis in skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import { useMediaQuery } from 'usehooks-ts'
 import { motion } from 'framer-motion'
 
-const skills = ['Typescript','React JS','Express JS', 'Node JS', 'Next JS', 'Tailwind CSS', 'ShadCN UI', 'Framer Motion', 'Zustand','Prisma', 'Postgresql',
- 'Tanstack Query', 'Zod', 'Socket IO', 'Next Auth', 'Docker'
+const skills = ['Typescript','React JS','Express JS', 'Node JS', 'Next JS', 'tRPC', 'Tailwind CSS', 'ShadCN UI', 'Framer Motion', 'Zustand','Prisma', 'Postgresql',
+ 'Tanstack Query', 'Zod', 'Socket IO', 'Next Auth', 'Docker', 'Redis'
 ]
 
 export default function Skills() {
@@ -19,11 +19,11 @@ export default function Skills() {
             const file = skill === 'Next Auth' ? '.png' : '.svg'
 
             return <motion.li initial={{opacity: 0, scale: 0.7}} whileInView={{opacity: 1, scale: 1}} transition={{duration: 0.3, delay: 0.1 * i,type: 'spring', bounce: 0.6}}
-            key={skill} className="flex flex-col items-center font-semibold text-lg uppercase gap-2"> 
+            key={skill} className="flex flex-col items-center font-semibold text-lg gap-2"> 
                 <div className="bg-white/10 to-transparent rounded-lg p-4 mb:p-2">
                   <Image src={'/' + image + file} width={isMobile ? 50 : 60} height={isMobile ? 50 : 60} alt={skill}/>
                 </div>
-                <h3 className="truncate mb:text-sm">{skill}</h3>
+                <h3 className="truncate uppercase mb:text-sm">{skill}</h3>
             </motion.li>
           })}
        </ul>
